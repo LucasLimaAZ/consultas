e108dd58b215e3cff08122cd2e182a1a9a57b1b9
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2020-06-17 18:00:06 -0300

    api

--- src/components/Patients/storePatient.js
+++ src/components/Patients/storePatient.js
@@ -12,6 +12,7 @@ const StorePatients = props => {
     const [cpf, setCpf] = useState("")
     const [currency, setCurrency] = useState("")
     const [dateColor, setDateColor] = useState("form-control input nascimento")
+    const [body, setBody] = useState({})
 
     useEffect(() => {
         props.setPageTitle("Cadastrar Paciente")
@@ -19,6 +20,7 @@ const StorePatients = props => {
 
     const handleCpf = e => {
         setCpf(cpfMask(e.target.value))
+        setBody(e.target.value)
     }
 
     const handleCurrency = e => {
@@ -29,8 +31,23 @@ const StorePatients = props => {
         setDateColor("form-control input")
     }
 
+    const handleChange = e => {
+        let value = e.target.value
+        let name = e.target.name
+
+        setBody({
+            ...body,
+            [name]: value
+        })
+    }
+
+    const handleFormSubmit = e => {
+        e.preventDefault()
+        console.log(body)
+    }
+
     return(
-        <>
+        <form onSubmit={handleFormSubmit}>
         <Row>
             <Col md={{size: 6}}>
                 <div className="box">
@@ -40,23 +57,23 @@ const StorePatients = props => {
                     <Row>
                         <Col md={8}>
                             <label htmlFor="name">Nome: </label>
-                            <input type="text" id="name" placeholder="Nome completo" className="form-control input" />
+                            <input onChange={handleChange} type="text" name="name" placeholder="Nome completo" className="form-control input" />
                         </Col>
                         <Col md={4}>
                             <label htmlFor="landline">Telefone Fixo: </label>                                    
-                            <input type="text" id="landline" className="form-control input" placeholder="DDD + número"/>
+                            <input onChange={handleChange} type="text" name="landline" className="form-control input" placeholder="DDD + número"/>
                         </Col>
                     </Row>
                     <Row style={{marginTop: '32px'}}>
                         <Col md={4}>
                             <label htmlFor="phone">Celular: </label>
-                            <input type="text" id="phone" className="form-control input" placeholder="DDD + número"/>
+                            <input onChange={handleChange} type="text" name="phone" className="form-control input" placeholder="DDD + número"/>
                         </Col>
                         <Col md={4}>
                             <label htmlFor="cpf">CPF: </label>
                             <input 
                                 type="text" 
-                                id="cpf" 
+                                name="cpf" 
                                 className="form-control input"
                                 onChange={handleCpf} 
                                 maxLength="14" 
@@ -66,7 +83,7 @@ const StorePatients = props => {
                         </Col>
                         <Col md={4}>
                             <label htmlFor="rg">RG: </label>
-                            <input type="text" id="rg" className="form-control input"/>
+                            <input onChange={handleChange} type="text" name="rg" className="form-control input"/>
                         </Col>
                     </Row>
                     <Row style={{marginTop: '32px', marginBottom: '32px'}}>
@@ -74,7 +91,7 @@ const StorePatients = props => {
                             <label htmlFor="birthday">Data de Nascimento: </label>
                             <input 
                                 type="date" 
-                                id="birthday" 
+                                name="birthday" 
                                 className={dateColor}
                                 placeholder="Data de Nascimento" 
                                 onChange={handleDateChage}
@@ -106,21 +123,21 @@ const StorePatients = props => {
                     <Row>
                         <Col md={6}>
                             <label htmlFor="street">Logradouro: </label>
-                            <input type="text" id="street" className="form-control input" placeholder="Rua, AV, etc..." />
+                            <input onChange={handleChange} type="text" name="street" className="form-control input" placeholder="Rua, AV, etc..." />
                         </Col>
                         <Col md={6}>
                             <label htmlFor="numero">Número: </label>
-                            <input type="number" id="numero" className="form-control input" />
+                            <input onChange={handleChange} type="number" name="numero" className="form-control input" />
                         </Col>
                     </Row>
                     <Row style={{marginTop: '32px', marginBottom: '32px'}}>
                         <Col md={6}>
                             <label htmlFor="neighborhood">Bairro: </label>                                    
-                            <input type="text" id="neighborhood" className="form-control input"/>
+                            <input onChange={handleChange} type="text" name="neighborhood" className="form-control input"/>
                         </Col>
                         <Col md={6}>
                             <label htmlFor="cep">CEP: </label>
-                            <input type="text" id="cep" className="form-control input" />
+                            <input onChange={handleChange} type="text" name="cep" className="form-control input" />
                         </Col>
                     </Row>
                     <Row style={{marginBottom: '32px'}}>
@@ -137,7 +154,7 @@ const StorePatients = props => {
                         </Col>
                         <Col md={6}>
                             <label htmlFor="state">UF: </label>                                    
-                            <select id="state" className="form-control input">
+                            <select name="state" className="form-control input">
                                 <option value="ACRE">ACRE</option>
                                 <option value="ALAGOAS">ALAGOAS</option>
                                 <option value="AMAPÁ">AMAPÁ</option>
@@ -180,17 +197,17 @@ const StorePatients = props => {
                     <Row>
                         <Col md={6}>
                             <label htmlFor="emergency_contact">Contato de Emergência: </label>                                    
-                            <input type="text" id="emergency_contact" className="form-control input" placeholder="DDD + número" />
+                            <input onChange={handleChange} type="text" name="emergency_contact" className="form-control input" placeholder="DDD + número" />
                         </Col>
                         <Col md={6}>
                             <label htmlFor="emergency_name">Nome: </label>
-                            <input type="text" id="emergency_name" className="form-control input" placeholder="Nome completo" />                               
+                            <input onChange={handleChange} type="text" name="emergency_name" className="form-control input" placeholder="Nome completo" />                               
                         </Col>
                     </Row>
                     <Row style={{marginTop: '32px', marginBottom: '32px'}}>
                         <Col md={12}>
                             <label htmlFor="observation">Observação: </label>                                    
-                            <textarea className="form-control input" id="observation"></textarea>
+                            <textarea onChange={handleChange} className="form-control input" name="observation"></textarea>
                         </Col>
                     </Row>
                 </div>
@@ -205,7 +222,7 @@ const StorePatients = props => {
                     <Row style={{marginBottom: '32px'}}>
                         <Col md={6}>
                             <label htmlFor="value">Valor: </label>                                    
-                            <input type="text" value={currency} onChange={handleCurrency} className="form-control input" id="value" />
+                            <input onChange={handleChange} type="text" value={currency} onChange={handleCurrency} className="form-control input" name="value" />
                         </Col>
                         <Col md={6}>
                             Deseja recibo?
@@ -234,17 +251,17 @@ const StorePatients = props => {
                     <Row>
                         <Col md={6}>
                             <label htmlFor="login">Usuário: </label>                                    
-                            <input type="text" className="form-control input" id="login" />
+                            <input onChange={handleChange} type="text" className="form-control input" name="login" />
                         </Col>
                         <Col md={6}>
                             <label htmlFor="password">Senha: </label>                                    
-                            <input type="password" className="form-control input" id="password" />
+                            <input onChange={handleChange} type="password" className="form-control input" name="password" />
                         </Col>
                     </Row>
                     <Row style={{marginTop: '32px', marginBottom: '32px'}}>
                         <Col md={6}>
                             <label htmlFor="role">Hierarquia: </label>                                    
-                            <select className="form-control input" id="role">
+                            <select className="form-control input" name="role">
                                 <option>Teste 1</option>
                                 <option>Teste 2</option>
                                 <option>Teste 3</option>
@@ -272,11 +289,11 @@ const StorePatients = props => {
             <Col md={12}>
                 <div className="box">
                     <h1 className="subtitle">Salvar dados</h1>
-                    <Button>Salvar</Button>
+                    <Button type="submit">Salvar</Button>
                 </div>
             </Col>
         </Row>
-        </>
+        </form>
     )
 }
 
