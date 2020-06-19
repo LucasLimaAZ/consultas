6421a5fe8843c11d2b2528bb0c64d616d6f9030d
Lucas de Lima Monteiro <lucasdelimamonteiro@gmail.com>
2020-06-18 22:08:17 -0300

    loader

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,6 +3,7 @@ import auth from '../../auth'
 import { Container, Row, Col, Button } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHospitalAlt } from '@fortawesome/free-solid-svg-icons'
+import Loader from 'react-loader-spinner'
 import './style.scss'
 
 const Login = props => {
@@ -10,13 +11,21 @@ const Login = props => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [incorrectMessage, setIncorrectMessage] = useState(false)
+    const [loader, setLoader] = useState(false)
 
     const handleLogin = e => {
+        setLoader(true)
         e.preventDefault()
         
         let data = {email: email, password: password}
 
-        auth.login(data, () => props.history.push("/"), () => setIncorrectMessage(true))
+        auth.login(data, 
+            () => props.history.push("/"), 
+            () => {
+                setIncorrectMessage(true)
+                setLoader(false)
+            }
+        )
     }
 
     return(
@@ -27,6 +36,7 @@ const Login = props => {
                         <h2><FontAwesomeIcon icon={faHospitalAlt} /> Acessar</h2>
                         <input onChange={e => setEmail(e.target.value)} type="email" className="form-control" name="user" required />
                         <input onChange={e => setPassword(e.target.value)} type="password" className="form-control" name="password" required />
+                        { loader ? <Loader type="TailSpin" color="#ffffff" height={40} width={40} /> : "" }
                         { incorrectMessage ? <p>Usuário ou senha incorretos</p> : "" }
                         <Button className="login-button">Acessar</Button>
                     </form>
