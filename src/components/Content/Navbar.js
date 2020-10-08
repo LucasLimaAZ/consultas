a8d56c4d583d09b8e68c106ac41825789d7dee89
LucasLimaAZ <lucasdelimamonteiro@gmail.com>
2020-10-08 14:49:38 -0300

    removing toggle from sidebar

--- src/components/Content/Navbar.js
+++ src/components/Content/Navbar.js
@@ -2,35 +2,31 @@ import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Navbar, Button, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap'
-import { Link } from 'react-router-dom'
+import './style.scss'
 import { connect } from 'react-redux'
 
 const NavBar = props => {
-
-  const [isOpen, setOpen] = useState(true)
-  const toggle = () => setOpen(!isOpen)
-
-  useEffect(() => {
-    //console.log(props)
-  })
-  
   return (
-    <Navbar color="light" light className="navbar shadow-sm p-3 mb-5 bg-white rounded" expand="md">
+    <Navbar 
+      color="light" 
+      light 
+      className="navbar shadow-sm p-3 mb-5 bg-white rounded" 
+      expand="md"
+    >
       <Button color="info" onClick={props.toggle}>
         <FontAwesomeIcon icon={faBars}/>
       </Button>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="m-auto" navbar>
-          <NavItem>
-            <div className="page-title">
-              <h1 className="title">
-                {props.pageTitle}
-              </h1>
-            </div>
-          </NavItem>
+
+      <Nav className="m-auto" navbar>
+        <NavItem>
+          <div className="page-title">
+            <h1 className="title">
+              {props.pageTitle}
+            </h1>
+          </div>
+        </NavItem>
       </Nav>
-      </Collapse>
+
     </Navbar>
   )
 }
